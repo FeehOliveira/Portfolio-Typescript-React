8c1991ab2f254a7213bb6d3c024089d1d42f6338
Felipe Oliveira <feeholiveira10@outlook.com>
2024-02-27 05:38:56 -0300

    responsividade e breakpoints

--- src/assets/pages/Home/sections/Hero/Hero.tsx
+++ src/assets/pages/Home/sections/Hero/Hero.tsx
@@ -1,29 +1,46 @@
-import { Container, Grid, Typography, styled } from "@mui/material"
+import { Button, Container, Grid, Typography, styled } from "@mui/material"
 import perfil from "../../../../images/perfil.jpg"
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import MailIcon from '@mui/icons-material/Mail';
 
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        height: "100vh"
     }))
 
     const StyledImg = styled("img")(() => ({
-        width: "30%",
+        width: "100%",
         borderRadius: "50%"
     }))
 
     return (
       <>
         <StyledHero>
-          <Container>
+          <Container maxWidth="lg">
             <Grid container spacing={2}>
-                <Grid item xs={4}>
+                <Grid item xs={12} md={4}>
                 <StyledImg src={perfil} />
                 </Grid>
-                <Grid item xs={8}>
-                    <Typography color="primary" variant="h1" >Felipe Oliveira</Typography>
-                    <Typography color="primary" variant="h2" >Desenvolvedor Front-End</Typography>
+                <Grid item xs={12} md={8}>
+                    <Typography color="primary" variant="h1" textAlign="center" >Felipe Oliveira</Typography>
+                    <Typography color="primary" variant="h2" textAlign="center" >Desenvolvedor Front-End</Typography>
+                    <Grid container>
+                        <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                        <Button>
+                    <DownloadForOfflineIcon />
+                        Download CV
+                        </Button>
+                        </Grid>
+                        <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                        <Button>
+                    <MailIcon/>
+                        Contact me
+                        </Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
             </Container>
